93e377aa48826febcf4d91e92f57d8daeb3b7c44
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-07 20:53:27 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,8 @@
 class Cat
-  
-  attr_accessor  :name 
-  
-  def initialize(NAME)
-    @NAME = NAME 
-  end 
-    
+  attr_reader :name
+  attr_accessor :mood
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
-
